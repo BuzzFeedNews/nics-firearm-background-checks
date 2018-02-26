f538c218378aa3c5251f95098771da821eca6fe4
Jeremy Singer-Vine <jsvine@gmail.com>
2018-02-26 16:39:33 -0500

    Update chart-scripts and charts for new Seaborn

--- scripts/chart-total-checks-36-months.py
+++ scripts/chart-total-checks-36-months.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-import pandas as pd
-import seaborn as sb
 import sys, os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+sb.set()
 
 checks = pd.read_csv(sys.stdin)
 
@@ -21,7 +23,8 @@ ax.set_title("NICS Background Check Totals — Past 36 Months", fontsize=24)
 ax.set_yticklabels([ "{0:,.0f}".format(y) for y in ax.get_yticks() ], fontsize=12)
 ax.set_xticks(tick_placement)
 ax.set_xticklabels([ totals.index[i] for i in tick_placement ])
-sb.mpl.pyplot.setp(ax.get_xticklabels(), rotation=0, fontsize=12)
+ax.set_xlim(0, len(totals) - 1)
+plt.setp(ax.get_xticklabels(), rotation=0, fontsize=12)
 ax.set_xlabel("")
 
-sb.plt.savefig(sys.stdout)
+plt.savefig(sys.stdout.buffer)
--- scripts/chart-total-checks-all.py
+++ scripts/chart-total-checks-all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
-import pandas as pd
-import seaborn as sb
 import sys, os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+sb.set()
 
 checks = pd.read_csv(sys.stdin)
 
@@ -13,8 +15,10 @@ ax = totals.plot(kind="area", figsize=(12, 8), color="#000000", alpha=0.5)
 ax.figure.set_facecolor("#FFFFFF")
 ax.set_title("Monthly NICS Background Check Totals Since Nov. 1998", fontsize=24)
 ax.set_yticklabels([ "{0:,.0f}".format(y) for y in ax.get_yticks() ], fontsize=12)
-sb.plt.setp(ax.get_xticklabels(), rotation=0, fontsize=12)
+plt.setp(ax.get_xticklabels(), rotation=0, fontsize=12)
 ax.set_xticks(tick_placement)
 ax.set_xticklabels([ totals.index[i].split("-")[0] for i in tick_placement ])
+ax.set_xlim(0, len(totals) - 1)
 ax.set_xlabel("")
-sb.plt.savefig(sys.stdout)
+
+plt.savefig(sys.stdout.buffer)
