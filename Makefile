e22ffd375cd274efd16ef382ad22159e221248e5
Jeremy Singer-Vine <jsvine@gmail.com>
2023-06-05 13:46:17 -0400

    Update parse-pdf.py for NICS's format changes

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ pdfs/nics_firearm_checks_-_month_year_by_state_type.pdf: now
 	curl -s "https://www.fbi.gov/file-repository/nics_firearm_checks_-_month_year_by_state_type.pdf" > $@
 
 data/nics-firearm-background-checks.csv: now
-	python scripts/parse-pdf.py < pdfs/nics_firearm_checks_-_month_year_by_state_type.pdf > $@
+	python scripts/parse-pdf.py pdfs/nics_firearm_checks_-_month_year_by_state_type.pdf > $@
 
 charts: now
 	python scripts/chart-total-checks-36-months.py < data/nics-firearm-background-checks.csv > charts/total-checks-36-months.png
--- scripts/parse-pdf.py
+++ scripts/parse-pdf.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
-import pandas as pd
 import datetime
-import pdfplumber
-from pdfplumber.utils import within_bbox, extract_text
+import re
+import sys
 from operator import itemgetter
-import sys, os
+
+import pandas as pd
+import pdfplumber
+from pdfplumber.utils import extract_text
 
 COLUMNS = [
     "month",
@@ -33,91 +35,120 @@ COLUMNS = [
     "return_to_seller_handgun",
     "return_to_seller_long_gun",
     "return_to_seller_other",
-    "totals"
+    "totals",
 ]
 
+
 def parse_month(month_str):
-    d = datetime.datetime.strptime(month_str, "%B - %Y")
+    normed = month_str.replace("-", " ")
+    d = datetime.datetime.strptime(normed, "%B %Y")
     return d.strftime("%Y-%m")
 
+
 def validate_data(checks):
-    try:
-        assert(len(checks) > 0)
-    except:
+    if not len(checks):
         raise Exception("No data found.")
 
-    ## Test vertical totals
-    # [2:] because first two columns are month and state name
+    # Test vertical totals
+    #
+    # Here, [2:] because first two columns are month and state name
     for c in COLUMNS[2:]:
         v_total = checks[c].iloc[-1]
         v_colsum = checks[c].sum()
-        try:
-            assert(v_colsum == (v_total * 2))
-        except:
+        if v_colsum != (v_total * 2):
             raise Exception("Vertical totals don't match on {0}.".format(c))
 
-    ## Test horizontal totals
+    # Test horizontal totals
     h_colsums = checks.sum(axis=1, numeric_only=True)
     h_totals = checks["totals"].fillna(0)
     zipped = zip(checks["state"], h_colsums, h_totals)
     for state, h_colsum, h_total in zipped:
-        try:
-            assert(h_colsum == (h_total * 2))
-        except:
+        if h_colsum != (h_total * 2):
             raise Exception("Horizontal totals don't match on {0}.".format(state))
 
+
 def parse_value(x):
-    if pd.isnull(x) or x == "": return None
-    return int(x.replace(",", ""))
+    if pd.isnull(x) or x == "":
+        return None
+    return int(re.sub(r"[^\d]", "", x))
+
+
+def parse_table_old(page):
+    cropped = page.crop(
+        (
+            0,
+            page.search("Alabama")[0]["top"],
+            page.width,
+            page.search("Totals")[-1]["bottom"] + 5,
+        )
+    )
+
+    edge_xs = list(set(map(itemgetter("x0"), cropped.edges)))
+    leftmost_char = min(map(itemgetter("x0"), cropped.chars))
+
+    return cropped.extract_table(
+        {
+            "horizontal_strategy": "text",
+            "vertical_strategy": "explicit",
+            "explicit_vertical_lines": [leftmost_char] + edge_xs,
+            "intersection_tolerance": 5,
+            "text_y_tolerance": 0,
+            "text_x_tolerance": 2,
+        }
+    )
+
+
+def parse_table_new(page):
+    cropped = page.crop(
+        (
+            0,
+            page.search("Alabama")[0]["top"] - 5,
+            page.width,
+            page.height,
+        )
+    )
+
+    return cropped.extract_table(
+        {"horizontal_strategy": "lines_strict", "vertical_strategy": "lines_strict"}
+    )
+
 
 def parse_page(page):
-    month_chars = [ c for c in page.chars if c["non_stroking_color"] == (1, 0, 0) ]
-    if len(month_chars) == 0:
-        return None
+    reds = [
+        [1, 0, 0],
+        (1, 0, 0),
+    ]
+    month_chars = [c for c in page.chars if c["non_stroking_color"] in reds]
+    assert len(month_chars)
 
     month_text = extract_text(month_chars, x_tolerance=2)
     month = parse_month(month_text)
     sys.stderr.write("\r" + month)
 
-    table_crop = page.crop((
-        0,
-        [ w for w in page.extract_words() if w["text"] == "State" ][0]["bottom"],
-        page.width,
-        page.rects[-1]["bottom"],
-    ))
+    table_parser = parse_table_new if month > "2022" else parse_table_old
+    _table = table_parser(page)
+    page.flush_cache()
 
-    edge_xs = list(set(map(itemgetter("x0"), table_crop.edges)))
-    leftmost_char = min(map(itemgetter("x0"), table_crop.chars)) 
-
-    _table = table_crop.extract_table({
-        "horizontal_strategy": "text",
-        "vertical_strategy": "explicit",
-        "explicit_vertical_lines": [ leftmost_char ] + edge_xs,
-        "intersection_tolerance": 5,
-        "text_y_tolerance": 0,
-        "text_x_tolerance": 2,
-    })
-
-    table = pd.DataFrame([ [ month ] + row for row in _table ])
+    table = pd.DataFrame([[month] + row for row in _table if any(row)])
+    assert len(table)
 
     table.columns = COLUMNS
     table[table.columns[2:]] = table[table.columns[2:]].applymap(parse_value)
 
-    table.loc[(table["state"] == "llinois"), "state"] = "Illinois"
-    table = table.loc[lambda df: df["state"].fillna("").str.strip() != ""]
-    try: validate_data(table)
-    except: raise Exception("Invalid data for " + month)
+    validate_data(table)
 
     return table
 
-def parse_pdf(pdf):
-    checks_dfs = [ df for df in map(parse_page, pdf.pages) if df is not None ]
-    checks = pd.concat(checks_dfs).reset_index(drop=True)
 
-    return checks[checks["state"] != "Totals"]
+def parse_pdf(pdf):
+    checks_dfs = [parse_page(page) for page in pdf.pages if page.page_number > 1]
+    checks = pd.concat(checks_dfs)
+
+    return checks[~checks["state"].str.contains("Total")]
+
 
 if __name__ == "__main__":
-    with pdfplumber.open(sys.stdin.buffer) as pdf:
+    with pdfplumber.open(sys.argv[1]) as pdf:
         checks = parse_pdf(pdf)
 
     checks.to_csv(sys.stdout, index=False, float_format="%.0f")
